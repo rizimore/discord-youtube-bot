bbdcca5781d557919b170473476db3e427009cbe
rizimore <rizimore@gmail.com>
2021-02-01 13:56:51 +0500

    discord bot development

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
-require("dotenv").config();
+require('dotenv').config();
 
-console.log(process.env.DISCORD_BOT_TOKEN)
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const PREFIX = "-";
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    if (message.author.bot) return;
+
+    if (message.content.startsWith(PREFIX)) {
+        const [COMMAND_NAME, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
+
+        if (COMMAND_NAME === 'yt') {
+            console.log(args);
+        }
+    }
+});
+
+client.login('ODA1NzExMjY1OTUwMzM0OTk3.YBe3Fw.bWZbOcoRuLtKjK1VVmjOAIjhmtw');
