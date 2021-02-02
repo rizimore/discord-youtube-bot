f3d2ff64672ebe7dfa05b7bdce493ae264e22251
rizimore <rizimore@gmail.com>
2021-02-02 05:19:38 +0500

    discord bot development

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const PREFIX = "-";
+const YouTube = require('youtube-node');
+const youtube = new YouTube();
+youtube.setKey(process.env.YOUTUBE_KEY);
+
+let PREFIX = "-";
+let COUNT = 0;
+let YOUTUBE_DATA;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -14,11 +20,20 @@ client.on('message', message => {
 
     if (message.content.startsWith(PREFIX)) {
         const [COMMAND_NAME, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
+        const COMMAND_CONTENT = args.join(' ');
 
         if (COMMAND_NAME === 'yt') {
-            console.log(args);
+            youtube.search(COMMAND_CONTENT, 20, function(error, result) {
+                YOUTUBE_DATA = result;
+                if (!error) {
+                    message.channel.send('--7up https://www.youtube.com/watch?v=' + YOUTUBE_DATA.items[COUNT].id.videoId);
+					console.log(COMMAND_CONTENT);
+                } else {
+                    message.reply('Something is not working fine, contact developer please!');
+                }
+            });
         }
     }
 });
 
-client.login('ODA1NzExMjY1OTUwMzM0OTk3.YBe3Fw.bWZbOcoRuLtKjK1VVmjOAIjhmtw');
+client.login(process.env.DISCORD_BOT_TOKEN);
